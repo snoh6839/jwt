c144138921b70dfb1d71d2a22fb9fdc27b650264
SNOH <116703016+snoh6839@users.noreply.github.com>
2023-08-03 18:02:30 +0900

    vue install

---
+++ frontend/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div><button @click="login()">login</button></div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      token: '',
+    }
+  },
+  methods: {
+    login(){
+      axios.get('http://localhost:8000/api/token?id=ppp')
+      .then(res => {
+        console.log(res.data);
+        this.token = res.data.token;
+      })
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
